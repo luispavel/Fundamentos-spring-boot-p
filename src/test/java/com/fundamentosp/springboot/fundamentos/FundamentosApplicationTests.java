e4d78df8860dcbebad08b0136eba81e36f1f898f
LuisPavel <luispavelvarela@gmail.com>
2022-05-18 23:49:18 -0500

    first commit with files

---
+++ src/test/java/com/fundamentosp/springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosp.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
